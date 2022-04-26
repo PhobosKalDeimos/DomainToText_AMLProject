8317e9e8bb2f00932339df4199718b39f68408e7
PhobosKalDeimos <98160430+PhobosKalDeimos@users.noreply.github.com>
2022-04-26 12:00:11 +0200

    Update config_default.py

--- DescribingTextures/models/triplet_match/config_default.py
+++ DescribingTextures/models/triplet_match/config_default.py
@@ -8,7 +8,7 @@ C.TRAIN_SPLIT = 'train'
 C.EVAL_SPLIT = 'val'
 
 C.LOAD_WEIGHTS = '/content/DomainToText_AMLProject/outputs/triplet_match/BEST_checkpoint.pth'
-C.INIT_WORD_EMBED = 'fast_text'  # rand / fast_text
+C.INIT_WORD_EMBED = 'rand'  # rand / fast_text
 C.LANG_INPUT = 'phrase'  # description
 
 # config for model
